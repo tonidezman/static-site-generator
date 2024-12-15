4bbeac9784d3ff71273a726b323a6f370d02dbf2
Toni Dezman <toni.dezman@gmail.com>
2024-12-15 08:55:55 +0100

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import TextNode, TextType
+
+def main():
+    print(TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev"))
+
+if __name__ == "__main__":
+    main()
---
+++ src/textnode.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+class TextType(Enum):
+    text = 1
+    BOLD = 2
+    ITALIC = 3
+    CODE = 4
+    LINK = 5
+    IMAGE = 6
+
+
+class TextNode:
+    def __init__(self, text: str, text_type: TextType, url: str = None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"

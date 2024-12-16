40ee3d32f3526146c3dda60d562c1913a81154cd
Toni Dezman <toni.dezman@gmail.com>
2024-12-16 10:29:39 +0100

    copy from static to public folder

--- src/main.py
+++ src/main.py
@@ -1,7 +1,36 @@
+import os
+import shutil
 from textnode import TextNode, TextType
 
 def main():
-    print(TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev"))
+    copy_static_files_to_public_folder()
+
+def copy_static_files_to_public_folder():
+    if not os.path.exists("public"):
+        os.makedirs("public")
+
+    if os.path.exists("public"):
+        for item in os.listdir("public"):
+            item_path = os.path.join("public", item)
+            if os.path.isfile(item_path):
+                os.remove(item_path)
+            elif os.path.isdir(item_path):
+                shutil.rmtree(item_path)
+
+    for root, dirs, files in os.walk("static"):
+        relative_root = os.path.relpath(root, "static")
+        for directory in dirs:
+            dest_dir = os.path.join("public", relative_root, directory)
+            if not os.path.exists(dest_dir):
+                os.makedirs(dest_dir)
+
+        for file in files:
+            src_path = os.path.join(root, file)
+            relative_root = os.path.relpath(root, "static")
+            dest_path = os.path.join("public", relative_root, file)
+            shutil.copy(src_path, dest_path)
+    
+    
 
 if __name__ == "__main__":
     main()
